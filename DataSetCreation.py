0743ac6c876d440862694a58bfc69116883bd77a
Naveen Bandarage <thebanterage@gmail.com>
2021-03-05 11:25:41 +1300

    Trying to set up how to best read the dataset. Stopping for lunch.

--- DataSetCreation.py
+++ DataSetCreation.py
@@ -8,3 +8,27 @@ class config:
     batch_size = 32
     sequence_len = 30
     model_path = 'lm_lrdecay_drop.bin'
+
+#creating a dataset from which we can use
+
+
+def create_dataset(synopsis, batch_size, seq_len):
+    np.random.seed(0)
+
+    synopsis = synopsis.apply(lambda x: str(x).lower()).values
+    synopsis_text = ' '.join(synopsis)
+
+    tokens = config.tokenizer(synopsis_text)
+    global num_batches
+    num_batches = int(len(tokens)/(seq_len*batch_size))
+    tokens = tokens[:num_batches*batch_size*seq_len]
+
+    words = sorted(set(tokens))
+    #w2i = word two index and i2w = index to word.
+    w2i = {w: i for i, w in enumerate(words)}
+    i2w = {i: w for i, w in enumerate(words)}
+
+    tokens = [w2i[tok] for tok in tokens]
+    target = np.zeros_like((tokens))
+    target[:-1] = tokens[1:]
+    target[-1] = tokens[0]
